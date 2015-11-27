401115817b256810d1fb7765c1ca5a7d20a296ee
jirenne <jindriskarenn@gmail.com>
2015-11-27 21:09:33 +0100

    Create interpret.h

---
+++ interpret.h
@@ -0,0 +1,14 @@
+/*
+* Projekt: Interpret jazyka IFJ15
+* Subor: interpret.c
+* Popis: Interpret
+* Autor: Rennerová Jindřiška (xrenne00)
+*/
+#ifndef _INTERPRET_H_
+#define _INTERPRET_H_
+
+#include "libraries.h"
+#include "interpret.h"
+
+int inter(MainData * data, tListOfInstr * instrList);
+#endif
