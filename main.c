ddb291c5e187bc40ef26f447439a6cbd367352b7
Juraj <jurajsokol.s@gmail.com>
2015-12-01 14:02:05 +0100

    main s SA

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "scanner.h"
 #include "libraries.h"
+#include "sa_vyrazov.h"
 
 
 int main(int argc, char *argv[]){
@@ -7,19 +8,25 @@ int main(int argc, char *argv[]){
 		return 1;
 	FILE *fp;
 	tState tokenType;
+	char *vstup = NULL;
 	fp = fopen(argv[1],"r");
   	if (fp == NULL)
   	{
     	fprintf(stderr, "Open file error!\n");
     	return 1;
   	}
+
+	printf("SA výraz: %d\n", analyza(fp, vstup));
+	free(vstup);
 	while(1)
 	{
 		char *token = Token(&tokenType, fp);
 		if(tokenType == S_EOF)
 			break;
 	    printf("%s - %d\n", token, tokenType);
+			free(token);
 	}
+
 	fclose(fp);
 
 	return 0;
